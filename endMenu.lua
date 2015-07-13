69014cf0f62158d219c3447d0e5a8739a00aa1a5
CogentInvalid <pchipman@email.arizona.edu>
2015-07-12 20:50:38 -0700

    she DED
    lik dis if u cri evertim

--- endMenu.lua
+++ endMenu.lua
@@ -1,22 +1,30 @@
 endMenu = class:new()
 
 function endMenu:init()
-
+	self.lol = 0
+	self.haha = 0
+	self.rofl = 0
+	love.graphics.setBackgroundColor(255,255,255)
 end
 
 function endMenu:update(dt)
-
+	self.lol = self.lol + 1.8*dt
+	self.haha = math.sin(self.lol)
+	self.rofl = self.rofl - dt
+	if self.rofl < 0 then self.rofl = 0 end
 end
 
 function endMenu:draw()
 	love.graphics.setColor(255,255,255)
-	love.graphics.draw(img["deathscreen"], 0, 0)
+	love.graphics.draw(img["deathscreen"], 400, 300, self.haha*(0.6+self.rofl*20), 1, 1, 400, 300)
 end
 
 function endMenu:keypressed(key)
 	if key == "return" then
 		gameMode:start()
 		currentMode = gameMode
+	else
+		self.rofl = 1
 	end
 end
 
